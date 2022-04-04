4647a6765de7970a8f3fd4e963fd52e87e8b6a21
wolfhound-www <90396788+wolfhound-www@users.noreply.github.com>
2022-04-04 19:45:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ layui.use(['element', 'layer', 'util', 'table'], function(){
 	,util = layui.util
 	,table = layui.table
 	,$ = layui.$;
-	var search = "";
+
 	//头部事件
 	util.event('lay-header-event', {
 		//左侧菜单伸缩事件
@@ -24,23 +24,39 @@ layui.use(['element', 'layer', 'util', 'table'], function(){
 			}
 		}
 	});
-	//搜索框点击默认选中事件
+	//数据加载事件
+	$(".file-nav-item").click(function(){
+		table.reload('fileListRender',{url: 'json/'+this.id+'.json'});
+	});
+	//搜索框点击选中事件
 	$("#search").click(function(){
 		this.select();
 	});
 	//搜索事件
+	var search = ""
+	var colsData = [{field:'num', title: '序号', width:60, unresize:true, type:'numbers'}
+		,{field:'fileId', title: '文号', sort:true, width:200, unresize:true}
+		,{field:'fileName', title: '名称', sort:true, unresize:true, templet: '<div><a href="{{d.url}}"  target="_blank">{{d.fileName}}</a></div>'}
+		,{field:'fileTag', title: '标签', width:220, unresize:true
+			, templet: function(d){
+				var tags=d.fileTag.split(" ");
+				var fileTagDisplay="";
+				if (d.fileTag != ""){
+					tags.forEach(function(item){
+						fileTagDisplay+="<span class='layui-badge layui-bg-green tag'>"+item+"</span>";
+					});
+				}
+				return fileTagDisplay;
+		}}
+	]
 	$("#searchBtn").click(function(){
 		search = $("#search").val().replace(/(^\s*)|(\s*$)/g,"");
-		if (search != "") {
-			$("#searchTips").html("<span class='layui-badge keyword' style='background-color: transparent;'>当前搜索关键词：</span><span class='layui-badge layui-bg-green keyword'>"+search+"</span>");
+		if (search == "") {
+			colsData[2].title = '名称';
 		} else {
-			$("#searchTips").html("");
+			colsData[2].title = "名称<span class='layui-badge layui-bg-green keyword'>"+search+"</span>";
 		}
-		table.reload('fileListRender');
-	});
-	//数据加载事件
-	$(".file-nav-item").click(function(){
-		table.reload('fileListRender',{url: 'json/'+this.id+'.json'});
+		table.reload('fileListRender',{cols: [colsData]});
 	});
 	//表格渲染事件
 	table.render({
@@ -48,24 +64,10 @@ layui.use(['element', 'layer', 'util', 'table'], function(){
 		,id: 'fileListRender'
 		,url: "json/CN_CCAR.json"
 		,height: 'full-80'
-		,cols: [[
-			{field:'num', title: '序号', width:60, unresize:true, type:'numbers'}
-			,{field:'fileId', title: '文号', sort:true, width:200, unresize:true}
-			,{field:'fileName', title: '名称', sort:true, unresize:true, templet: '<div><a href="{{d.url}}"  target="_blank">{{d.fileName}}</a></div>'}
-			,{field:'fileTag', title: '标签', width:220, unresize:true
-				, templet: function(d){
-					var tags=d.fileTag.split(" ");
-					var fileTagDisplay="";
-					if (d.fileTag != ""){
-						tags.forEach(function(item){
-							fileTagDisplay+="<span class='layui-badge layui-bg-green tag'>"+item+"</span>";
-						});
-					}
-					return fileTagDisplay;
-			}}
-		]]
+		,cols: [colsData]
 		,done:function(res){
 			var that = this.elem.next();
+			this.cols = [];
 			res.data.forEach(function (item, index) {
 				if (search != "") {
 					if (item.fileId.search(new RegExp(search, 'i')) == -1 && item.fileName.search(new RegExp(search, 'i')) == -1 && item.fileTag.search(new RegExp(search, 'i')) == -1 && item.fileKeyword.search(new RegExp(search, 'i')) == -1) {
